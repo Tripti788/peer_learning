5dca4a3c1015a4ab8eedd580917a4a185c8adc40
Tripti788 <144474933+Tripti788@users.noreply.github.com>
2025-06-24 13:23:55 +0000

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const port = 3000
+
+const port = process.env.PORT || 3000;
 
 
 const authRoutes = require('./Routes/authRoutes');
